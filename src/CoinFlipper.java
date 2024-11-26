16a1c6203003f2ffc98ceab0c85b13ac0876d712
ericc346 <ericc346@nycstudents.net>
2024-11-26 13:03:32 -0500

    comments written

--- src/CoinFlipper.java
+++ src/CoinFlipper.java
@@ -13,7 +13,7 @@ public class CoinFlipper {
         this.playerTwo = playerTwo;
     }
 
-    public String[] flipCoin(int flipNumber)
+    public String[] flipCoin(int flipNumber) // creates an array with the coin flipped based on user inputted number
     {
         hort = new String[flipNumber];
 
@@ -31,7 +31,11 @@ public class CoinFlipper {
         return hort;
     }
 
-    public int getScore(String[] coinFlips, String[] userPredicts){
+    public int getScore(String[] coinFlips, String[] userPredicts)  //checks if index of the user predicts array
+                                                                    // equal coin flip array assign points randomly
+                                                                    // adds points to the score return score
+
+    {
         int score = 0;
         for(int i = 0; i < coinFlips.length; i++){
             if(coinFlips[i].equals(userPredicts[i])){                   // if user predict matches coin flip
@@ -49,7 +53,8 @@ public class CoinFlipper {
         return name + " has a score of " + score;                           // returns String
     }
 
-    public void winner(String name, String nameTwo, int score, int otherScore){
+    public void winner(String name, String nameTwo, int score, int otherScore) //prints winner
+    {
         System.out.println("Surprise! There's bonus points!");
         System.out.println("For each letter in your name, you get 1 point extra!");
         score += name.length();
@@ -67,28 +72,32 @@ public class CoinFlipper {
             System.out.println("No one wins! At least you have the same name");
         }
     }
-    public void coins(){
+    public void coins() //print the coin flipped array
+    {
         System.out.println("Correct Prediction: " + Arrays.toString(hort));  // turns array into string
     }
 
-    public void player(int score, int scoreTwo){
+    public void player(int score, int scoreTwo) //prints final score with player name
+    {
         System.out.println("Player 1: " + playerOne +" has score of " + (score + playerOne.length()));
         System.out.println("Player 2: " + playerTwo +" has score of " + (scoreTwo + playerOne.length()));
     }
-    public void playerOne(){
+    public void playerOne() //prints player one turn
+    {
         System.out.print("Player One Turn:");
     }
 
     public void playerTwo(){
         System.out.print("Player Two Turn:");
-    }
+    } //prints Player two turn
 
-    public void gameCelebration(boolean gameEnd){
+    public void gameCelebration(boolean gameEnd) //game end sequence
+    {
         int i = 0;
         while(gameEnd) {
             i++;
             System.out.println("!!!!!!!!!!!!!!!!!!!!!Game Over!!!!!!!!!!!!!!!!!!!!!!");
-            if(i > 5) {
+            if(i > 5) {                                                 //once i is greater than 5 the  while loop stops
                 gameEnd = false;
             }
         }
--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
         String[] userPredict = new String[flips];
 
         System.out.println();
-        help.playerOne();
+        help.playerOne();                                       // prints Player one turn
 
         for(int i = 0; i < flips ; i++){
             System.out.println();
@@ -35,39 +35,39 @@ public class Main {
 
         }
 
-        int score = (help.getScore(cars,userPredict));                      //gets player one score
+        int score = (help.getScore(cars,userPredict));                      //assign player one score to score
 
         System.out.println();
         System.out.println();
         System.out.println();
-        help.playerTwo();
+        help.playerTwo();                                                       // prints Player two turn
 
         for(int i = 0; i < flips ; i++){
             System.out.println();
             System.out.println("-------------");
             System.out.println("Current Coin Number: " + (i+1));
             System.out.print("Enter a heads or tails for coin number " + (i +1) +": ");
-            String prediction = askPredict.nextLine();
-            userPredict[i] = prediction.toLowerCase();
+            String prediction = askPredict.nextLine();                                  //ask for player two prediction
+            userPredict[i] = prediction.toLowerCase();                                    //puts in it an array
 
         }
 
         System.out.println();
         System.out.println("Player 1:" + help.toString(playerOne,score));
-        int scoreTwo = (help.getScore(cars,userPredict));
+        int scoreTwo = (help.getScore(cars,userPredict));             //assign player two score to scoreTwo
 
         System.out.println("Player 2:" + help.toString(playerTwo,scoreTwo));
         System.out.println();
 
-        help.winner(playerOne,playerTwo,score,scoreTwo);
+        help.winner(playerOne,playerTwo,score,scoreTwo);                // prints out winner
         System.out.println();
 
-        help.player(score,scoreTwo);
+        help.player(score,scoreTwo);                                    // print out final score
         help.coins();
 
         System.out.println();
         boolean gameEnd = true;
-        help.gameCelebration(gameEnd);
+        help.gameCelebration(gameEnd);                                  // prints out game end celebration 5 times
 
     }
 }
